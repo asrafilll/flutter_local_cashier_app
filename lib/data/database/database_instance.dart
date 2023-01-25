7cf57a83c80069e83e231a4713cf84b0076d83c5
asrafilll <asrafilr@gmail.com>
2023-01-26 01:48:38 +0700

    Done AdminBottomNav, continue Slicing AdminHomePage

---
+++ lib/data/database/database_instance.dart
@@ -0,0 +1,10 @@
+// import 'package:sqflite/sqflite.dart';
+// import 'package:path_provider/path_provider.dart';
+
+// class DatabaseInstance {
+
+// final String dbName = 'personal_cashier';
+//   var databasesPath = await getDatabasesPath();
+//   var path = join(databasesPath, dbName);
+
+// }
---
+++ lib/data/model/product.dart
@@ -0,0 +1,34 @@
+// const String tableProduct = 'product';
+// const String columnId = '_id';
+// const String columnName = 'name';
+// const String columnCategory = 'category';
+// const String columnPrice = 'price';
+// const String columnStock = 'stock';
+// const String columnImg = 'img';
+
+// class Product {
+//   int? id;
+//   String? name;
+//   String? category;
+//   int? price;
+//   int? stock;
+//   String? img;
+
+//   Product({
+//     this.id,
+//     this.name,
+//     this.category,
+//     this.price,
+//     this.stock,
+//     this.img,
+//   });
+
+//   Product.fromJson(Map<String, dynamic> json) {
+//     id = map[columnId];
+//     name = map[columnName];
+//     category = map[columnCategory];
+//     price = map[columnPrice];
+//     stock = map[columnStock];
+//     img = map[columnImg];
+//   }
+// }
